18ab10424989fc8c4174eedb1ea7358936c172a2
leogandmars <legoandmars@gmail.com>
2021-01-15 15:01:49 -0800

    chore: regen site

---
+++ docs/_next/static/aEXXfDmwC9nfz6hXCVTQm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[id]":[a,"static\u002Fcss\u002F0bb2f9aaf62dcef9d197.css"],"/index":[a],"/notes":[a],"/packs":[a],"/walls":[a]}}("static\u002Fchunks\u002F2e7bae88bcf0f04dcc2cec47579662c048989965.03d67e3ba46a7616fc12.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
